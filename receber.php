f132372d3da5d1a619bcb742c7a997b3b0100107
Larissa Belisse <larissabelisse@estudante.sc.senai.br>
2019-10-18 19:54:47 -0300

    aula07 - 18/10/2019
    Inicio da autenticaçao e entrada no sistema

--- receber.php
+++ receber.php
@@ -1,13 +1,32 @@
 <?php
 # Back-end
-
-echo "\n<pre>\n";
-echo "$_GET";
-echo "\n<pre>\n";
-
-if ($_GET('idade') < 18) {
-    echo '<h1 style="color:red;"> Não pode beber na festa</h1>';
-} else {
-    echo '<h1 style="color:green;"> Bebe pouco fii,
-    a vida continua!</h1>';
+//Verifica se o método GET está enviando dados
+if (isset($_GET['nome'])) {
+    echo "\n<h1>Envio de dados método <em>GET</em></h1>";
+    echo "\n<pre>\n";
+    print_r($_GET); #Array
+    echo "\n</pre>\n";
+    print("\n<br><strong>Nome: </strong>");
+    print("$_GET[nome]");
+    print("\n<br><strong>E-mail: </strong>");
+    print("$_GET[email]");
+    print("\n<br><strong>Senha: </strong>");
+    print("$_GET[senha]");
+    print("\n<br><strong>Data de Nascimento: </strong>");
+    print("$_GET[data_de_nascimento]");
+}
+//Verifica se o método POST está enviando dados
+if (isset($_POST['nome'])) {
+    echo "\n<h1>Envio de dados método <em>POST</em></h1>";
+    echo "\n<pre>\n";
+    print_r($_POST); #Array
+    echo "\n</pre>\n";
+    print("\n<br><strong>Nome: </strong>");
+    print("$_POST[nome]");
+    print("\n<br><strong>E-mail: </strong>");
+    print("$_POST[email]");
+    print("\n<br><strong>Senha: </strong>");
+    print("$_POST[senha]");
+    print("\n<br><strong>Data de Nascimento: </strong>");
+    print("$_POST[data_de_nascimento]");
 }
